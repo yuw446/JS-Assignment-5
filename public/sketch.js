d09700b9c5f69c3e1f202c792cf19e3a28bc480e
yuw446 <43831158+yuw446@users.noreply.github.com>
2020-11-26 18:33:26 -0500

    Sevrer up

--- public/sketch.js
+++ public/sketch.js
@@ -4,4 +4,5 @@ function setup() {
 
 function draw() {
   background(220);
+  ellipse(mouseX, mouseY, 60, 60);
 }
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+let express = require('express');
+
+let app = express();
+let server = app.listen(8080);
+
+app.use(express.static('public'));
+
 console.log("Node Server.js is Running.");
+
+let socket = requie('socket.io');
+
+let io = socket(server);
+
+io.sockets.on('connection', newConnection);
